62f7d41059dc35dec2e11e8b077df03503dd8199
zengweishuai <weishuai107@gmail.com>
2025-06-29 16:29:57 +0800

    Upload report and demonstration;

--- cfg/config.py
+++ cfg/config.py
@@ -1,7 +1,3 @@
-Gemini_API_KEY = 'AIzaSyB7QlCXU3Szprp05KsX665DmKglR6zM63Y'
-Gemini_MAX_TOKENS = 2048
-Gemini_TEMPERATURE = 1.3
-hist_cache_path = 'cache/hist.pkl'
 models = ['Deepseek-V3',
  'Deepseek-R1',
  'Gemini-2.0-flash',
